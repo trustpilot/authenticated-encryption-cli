639e067b6832aae72f8d0b8ebd2f8b49a2bd0da4
SÃ¸ren Pedersen <sh.pedersen@gmail.com>
2017-01-05 10:01:23 +0100

    Add class: Arguments

---
+++ Aem/Arguments.cs
@@ -0,0 +1,18 @@
+﻿namespace Aem
+{
+    public class Arguments
+    {
+        public Arguments(Command command, string message, bool urlEncode = false)
+        {
+            Command = command;
+            Message = message;
+            UrlEncode = urlEncode;
+        }
+
+        public Command Command { get; }
+
+        public string Message { get; set; }
+
+        public bool UrlEncode { get; }
+    }
+}
