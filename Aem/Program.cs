543ab94b9eb10f5f2738f47e6401dc1df3974c64
Søren Pedersen <sh.pedersen@gmail.com>
2017-01-05 09:57:53 +0100

    Extract configuration parsing

--- Aem/Program.cs
+++ Aem/Program.cs
@@ -11,6 +11,7 @@
     {
         public static void Main(string[] args)
         {
+            var configuration = ParseConfiguration();
             var urlEncode = false;
             var command = Command.Encrypt;
             ArgumentSyntax.Parse(args, syntax =>
@@ -21,31 +22,6 @@
                     syntax.DefineCommand("decrypt", ref command, Command.Decrypt, "Decrypt the given base64 encoded ciphertext");
                 });
 
-            var configFileName = "appSettings.json";
-            var currentDirectory = Directory.GetCurrentDirectory();
-            var appSettingsFilePath = Path.Combine(currentDirectory, configFileName);
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(currentDirectory)
-                .AddJsonFile(configFileName)
-                .AddJsonFile("appSettings.local.json", optional: true)
-                .Build();
-            var cryptKeyBase64 = configuration["cryptkey"];
-            var authKeyBase64 = configuration["authkey"];
-
-            if (string.IsNullOrWhiteSpace(cryptKeyBase64))
-            {
-                Console.Error.WriteLine($"error: please insert your cryptkey in the file: {appSettingsFilePath}");
-
-                Environment.Exit(1);
-            }
-
-            if (string.IsNullOrWhiteSpace(authKeyBase64))
-            {
-                Console.Error.WriteLine($"error: please insert your authkey in the file: {appSettingsFilePath}");
-
-                Environment.Exit(1);
-            }
-
             var message = Console.In.ReadToEnd();
 
             if (string.IsNullOrWhiteSpace(message))
@@ -55,8 +31,8 @@
                 Environment.Exit(1);
             }
 
-            var cryptKey = Convert.FromBase64String(cryptKeyBase64);
-            var authKey = Convert.FromBase64String(authKeyBase64);
+            var cryptKey = Convert.FromBase64String(configuration.CryptKeyBase64);
+            var authKey = Convert.FromBase64String(configuration.AuthKeyBase64);
 
             switch (command)
             {
@@ -78,6 +54,39 @@
             }
 
             Environment.Exit(0);
+        }
+
+        private static Configuration ParseConfiguration()
+        {
+            var configFileName = "appSettings.json";
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var appSettingsFilePath = Path.Combine(currentDirectory, configFileName);
+            var configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(currentDirectory)
+                .AddJsonFile(configFileName)
+                .AddJsonFile("appSettings.local.json", optional: true)
+                .Build();
+            var configuration = new Configuration
+                {
+                    AuthKeyBase64 = configurationRoot["authkey"],
+                    CryptKeyBase64 = configurationRoot["cryptkey"]
+                };
+
+            if (string.IsNullOrWhiteSpace(configuration.CryptKeyBase64))
+            {
+                Console.Error.WriteLine($"error: please insert your cryptkey in the file: {appSettingsFilePath}");
+
+                Environment.Exit(1);
+            }
+
+            if (string.IsNullOrWhiteSpace(configuration.AuthKeyBase64))
+            {
+                Console.Error.WriteLine($"error: please insert your authkey in the file: {appSettingsFilePath}");
+
+                Environment.Exit(1);
+            }
+
+            return configuration;
         }
 
         private enum Command
